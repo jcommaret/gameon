aedac7e8db03337023019f76d1fcd97a7e2bfc21
jcommaret@gmail.com <jcommaret@gmail.com>
2022-06-08 16:51:01 +0200

    Architecture

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,16 +1,6 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
