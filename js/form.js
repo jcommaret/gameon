9ed4ff9c988165137e3d5e94d65a04904a4fce73
jcommaret@gmail.com <jcommaret@gmail.com>
2022-06-20 19:06:50 +0200

    Index

--- js/form.js
+++ js/form.js
@@ -1,1 +1,9 @@
-const formData = document.querySelectorAll(".formData");
+let first=document.reservation.first.value;  
+
+function validateForm(){  
+  if (first==null || first=="" || first.length<2) {  
+    console.log('⛔️ not all conditions are met');
+  } else {
+    console.log('😀');
+  }  
+}
