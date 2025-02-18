a862486b73d7daa05b384a57c6c5afc16f0bc2d7
Jérôme Commaret <jcommaret@gmail.com>
2025-02-18 23:10:37 +0100

    Add favicon and minor import cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/header";
 import { Hero } from "./components/hero";
 import { Modal } from "./components/modal";
+
 import "./style/main.scss";
 
 function App() {
